8b8d4e97596bd244a937bc42601f7ffcac44fed9
lkilkenny1997 <lkilkenny97@gmail.com>
2025-01-01 21:48:20 +0000

    fix: generic name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Ascensor Task Manager</title>
+  <title>Task Manager</title>
   @vite(['resources/css/app.css', 'resources/js/app.ts'])
 </head>
 
